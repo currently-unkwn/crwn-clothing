67fb5c6ef0479001a2e18703a6b289904a5ebf64
Vladislav Kovac <vladkovac@Vladislavs-iMac.local>
2022-05-26 13:19:35 +0300

    adding sass styles to our categories and other four categories objects

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import "./categories.styles.scss";
+
 const App = () => {
   const categories = [
     {
       id: "1",
       title: "Hats",
+    },
+    {
+      id: "2",
+      title: "Jackets",
+    },
+    {
+      id: "3",
+      title: "Sneakers",
+    },
+    {
+      id: "4",
+      title: "Womens",
+    },
+    {
+      id: "5",
+      title: "Mens",
     },
   ];
 
