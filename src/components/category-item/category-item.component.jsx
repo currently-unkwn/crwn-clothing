6ccce7708ce264a344ed38d329fa316c4cc1dd27
Vladislav Kovac <vladkovac@Vladislavs-iMac.local>
2022-05-27 09:10:17 +0300

    importing category-item component into app

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -3,17 +3,19 @@ import "./category-item.styles.scss";
 const CategoryItem = ({ category }) => {
   const { imageUrl, title } = category;
 
-  <div className="category-container">
-    <div
-      className="background-image"
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    ></div>
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
 
-    <div className="category-body-container">
-      <h2>{title}</h2>
-      <p>Shop Now</p>
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default CategoryItem;
