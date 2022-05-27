1159f762c0ce783e5b83e15954f5121a421c89ca
Vladislav Kovac <vladkovac@Vladislavs-iMac.local>
2022-05-27 08:51:01 +0300

    building category-item componentt

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,7 +1,19 @@
 import "./category-item.styles.scss";
 
-const CategoryItem = () => {
-  return <div>Categoy Item</div>;
+const CategoryItem = ({ category }) => {
+  const { imageUrl, title } = category;
+
+  <div className="category-container">
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    ></div>
+
+    <div className="category-body-container">
+      <h2>{title}</h2>
+      <p>Shop Now</p>
+    </div>
+  </div>;
 };
 
 export default CategoryItem;
