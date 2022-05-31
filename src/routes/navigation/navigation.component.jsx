431fb16a84df885b8ebdccdff71e95de543b6646
Vladislav Kovac <vladkovac@Vladislavs-iMac.local>
2022-05-31 16:32:36 +0300

    setting up basis for nav component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,14 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div>
-      <div>
-        <h1>I Am Navigation Bar</h1>
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <div>Logo</div>
+        </Link>
+
+        <nav className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </nav>
       </div>
 
       <Outlet />
-    </div>
+    </Fragment>
   );
 };
 
