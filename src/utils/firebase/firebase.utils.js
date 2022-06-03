fbd68099a6867b53018ce5f80861e16df0975902
Vladislav Kovac <vladkovac@Vladislavs-iMac.local>
2022-06-03 10:29:36 +0300

    initializing a firebase config

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXIUkiE7GzVoLPolOyXzseEFGHYyOjoOo",
+  authDomain: "crwn-clothing-db-b4753.firebaseapp.com",
+  projectId: "crwn-clothing-db-b4753",
+  storageBucket: "crwn-clothing-db-b4753.appspot.com",
+  messagingSenderId: "329714758989",
+  appId: "1:329714758989:web:1830c7bcf5de237a2b7304",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
