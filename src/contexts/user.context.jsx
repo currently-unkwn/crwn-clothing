5676b45e34d848b6ff1e4c995970834cc6b75d24
Vladislav Kovac <vladkovac@Vladislavs-iMac.local>
2022-06-14 16:26:33 +0300

    adding onAuthStateChanged

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,9 +1,19 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
+import { onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({});
 
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      console.log(user);
+    });
+
+    return unsubscribe;
+  }, []);
 
   const value = { currentUser, setCurrentUser };
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
