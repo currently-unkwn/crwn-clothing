948842f02e814abb897d242a5f1c880c01793381
Vladislav Kovac <vladkovac@Vladislavs-iMac.local>
2022-06-16 17:10:48 +0300

    feat: Initialize shop component and products context

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import { Routes, Route } from "react-router-dom";
+
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
-
-const Shop = () => {
-  return (
-    <div>
-      <div>
-        <h1>I Am Shop</h1>
-      </div>
-    </div>
-  );
-};
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
   return (
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -5,7 +5,10 @@ import {
   createUserDocumentFromAuth,
 } from "../utils/firebase/firebase.utils";
 
-export const UserContext = createContext({});
+export const UserContext = createContext({
+  currentUser: null,
+  setCurrentUser: () => null,
+});
 
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
