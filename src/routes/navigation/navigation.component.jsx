00a11272935435c5bbb43004f3eb11bce6061d35
Vladislav Kovac <vladkovac@Vladislavs-iMac.local>
2022-06-20 12:09:36 +0300

    feat: Add cart-icon, cart-dropdown components

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -7,6 +7,9 @@ import { UserContext } from "../../contexts/user.context";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import "./navigation.styles.scss";
 
 const Navigation = () => {
@@ -32,7 +35,10 @@ const Navigation = () => {
               Sign In
             </Link>
           )}
+
+          <CartIcon />
         </nav>
+        <CartDropdown />
       </div>
 
       <Outlet />
